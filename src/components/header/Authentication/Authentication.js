c20f6c99f8d974ad5e3db59fbc11df178550784a
Phat <hpl2761998@gmail.com>
2019-07-26 13:57:39 +0700

    delete unused code

--- src/components/header/Authentication/Authentication.js
+++ src/components/header/Authentication/Authentication.js
@@ -4,22 +4,7 @@ import UnAuthenticated from "./UnAuthenticated";
 import { UserContext } from "../../../contexts/UserContext";
 
 export default class Authentication extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { loggedIn: true };
-  // }
-
-  // componentWillMount() {
-  //   const token = JSON.parse(localStorage.getItem("Token"));
-  //   if (token === null) this.setState({ loggedIn: false });
-  // }
-
-  // changeState = value => {
-  //   this.setState({ loggedIn: value });
-  // };
-
   render() {
-    // const { loggedIn } = this.state;
     return (
       <UserContext.Consumer>
         {({ isLoggedIn }) => (
